29f4eec7bdba1e8ba5741c531ee0a5bbdd96e69b
snir <snir@snir-PC.pisoft.local>
2016-09-10 16:29:13 +0300

    added javadoc to compressor and decompressor

--- MyAlgorithm/src/io/MyCompressorOutputStream.java
+++ MyAlgorithm/src/io/MyCompressorOutputStream.java
@@ -3,19 +3,35 @@ package io;
 import java.io.IOException;
 import java.io.OutputStream;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MyCompressorOutputStream.
+ */
 public class MyCompressorOutputStream extends OutputStream {
 
+	/** The out. */
 	private OutputStream out;
 
+	/**
+	 * Instantiates a new my compressor output stream.
+	 *
+	 * @param out the out
+	 */
 	public MyCompressorOutputStream(OutputStream out) {
 		this.out = out;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.io.OutputStream#write(int)
+	 */
 	@Override
 	public void write(int arg0) throws IOException {
 		out.write(arg0);
 	}
 
+	/* (non-Javadoc)
+	 * @see java.io.OutputStream#write(byte[])
+	 */
 	public void write(byte[] bytes) throws IOException {
 		int i = 0;
 		int counter = 0;
@@ -32,6 +48,13 @@ public class MyCompressorOutputStream extends OutputStream {
 		}
 	}
 
+	/**
+	 * Byte counter.
+	 *
+	 * @param i the i
+	 * @param bytes the bytes
+	 * @return the int
+	 */
 	private int byteCounter(int i, byte[] bytes) {
 		int counter = 1;
 		while ((bytes.length < i) && (bytes[i] == bytes[i++])) {
--- MyAlgorithm/src/io/MyDecompressorInputStream.java
+++ MyAlgorithm/src/io/MyDecompressorInputStream.java
@@ -3,19 +3,35 @@ package io;
 import java.io.IOException;
 import java.io.InputStream;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MyDecompressorInputStream.
+ */
 public class MyDecompressorInputStream extends InputStream {
 	
+	/** The in. */
 	private InputStream in;
 	
+	/**
+	 * Instantiates a new my decompressor input stream.
+	 *
+	 * @param in the in
+	 */
 	public MyDecompressorInputStream(InputStream in) {
 		this.in = in;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.io.InputStream#read()
+	 */
 	@Override
 	public int read() throws IOException {
 		return (int)in.read();
 	}
 	
+	/* (non-Javadoc)
+	 * @see java.io.InputStream#read(byte[])
+	 */
 	public int read(byte[] bytes) throws IOException {
 		byte zeroOrOne;
 		byte counter;
